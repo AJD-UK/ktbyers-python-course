81301c62de6c0d3b9e7d345f9373fb037954f6cd
Andrew Devine <andrew@ajdnetworks.co.uk>
2017-09-23 21:15:21 +0100

    Add string

--- week1/ipv6_split.py
+++ week1/ipv6_split.py
@@ -2,6 +2,6 @@
 
 ipv6 = 'FE80:0000:0000:0000:0101:A3EF:EE1E:1719'
 ipv6_split = ipv6.split(':')
-print(ipv6_split)
+print ipv6_split
 ipv6_join = ':'.join(ipv6_split)
-print(ipv6)
+print "This is an IPv6 address %s" % ipv6
