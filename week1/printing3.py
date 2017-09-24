21bd50e863225525e3d4eee88a97517a8e81be9b
Andrew Devine <andrew@ajdnetworks.co.uk>
2017-09-24 08:29:23 +0100

    test

---
+++ week1/printing3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+router_name = "Fortigate"
+ip_addr = '1.2.3.4'
+print('My default gateway is the %s with IP address %s' % (router_name, ip_addr))
+print('My default gateway is the {0} with IP address {1}'.format(router_name, ip_addr))
+
+print('\nOperator\tOperation\tExample\tEvaluates to...\n--------------------------------------------------------')
+exponent_value = 2 ** 3
+print('**\t\tExponent\t2 ** 3\t%d' % exponent_value)
