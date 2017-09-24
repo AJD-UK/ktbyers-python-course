3e2c2c4b07f00b56edf780dbd6ae97d7b89130fc
Andrew Devine <andrew@ajdnetworks.co.uk>
2017-09-24 07:36:38 +0100

    test

--- week1/ipv6_split.py
+++ week1/ipv6_split.py
@@ -4,4 +4,4 @@ ipv6 = 'FE80:0000:0000:0000:0101:A3EF:EE1E:1719'
 ipv6_split = ipv6.split(':')
 print ipv6_split
 ipv6_join = ':'.join(ipv6_split)
-print "This is an IPv6 address %s" % ipv6
+print "This is an IPv6 address %s" % ipv6_join
--- week1/printing.py
+++ week1/printing.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
 router_name = "Fortigate"
-ip_addr = '1.2.3.4.'
+ip_addr = '1.2.3.4'
 print "My default gateway is the %s with IP address %s" % (router_name, ip_addr)
 print "My default gateway is the {0} with IP address {1}".format(router_name, ip_addr)
